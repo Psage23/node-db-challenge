94ba3865efd9c5527ea78f94edfb781f914b147b
Psage23 <pamelasage3@gmail.com>
2020-02-08 14:09:57 -0700

    added new files

---
+++ seeds/01-projects.js
@@ -0,0 +1,21 @@
+
+exports.seed = function(knex) {
+  return knex('projects')
+  .truncate()
+  .then(function() {
+    return knex('projects').insert([
+      {
+        project_name: 'Feed the Needy',
+        project_description: 'find food',
+        project_completed: 'false'
+      },
+
+      {
+        project_name: 'Travel to the Moon',
+        project_description: 'Find ways to travel',
+        project_completed: 'true'
+      }
+    ]);
+  })
+  
+};
---
+++ seeds/02-tasks.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex) {
+  return knex('tasks').insert([
+    {
+      project_id: 1,
+      task_id: 1,
+      task_name: 'Step 1',
+      descriptions: "This project is to help feed the needy"
+    }
+  ])
+    
+};
